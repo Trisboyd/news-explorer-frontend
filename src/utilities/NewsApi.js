4448dccc4d5584d4665b91d536055758e3dd9b31
Tristan Boyd <trisboyd@gmail.com>
2021-12-07 21:57:16 -0600

    'adjusted newsApi url'

--- src/utilities/NewsApi.js
+++ src/utilities/NewsApi.js
@@ -30,7 +30,7 @@ class NewsApi {
 
 
 const newsApi = new NewsApi({
-    baseUrl: "http://newsapi.org/v2/everything?",
+    baseUrl: "https://newsapi.org/v2/everything?",
     apiKey: "496286d2299947289df49c01001af364",
 })
 
