2b18041ccc5900fbf1c83cefaf26e9602073cc24
Tristan Boyd <trisboyd@gmail.com>
2021-12-07 22:10:51 -0600

    'adjusted newsApi url again'

--- src/utilities/NewsApi.js
+++ src/utilities/NewsApi.js
@@ -30,7 +30,7 @@ class NewsApi {
 
 
 const newsApi = new NewsApi({
-    baseUrl: "https://newsapi.org/v2/everything?",
+    baseUrl: "nomoreparties.co/news/v2/top-headlines?country=us",
     apiKey: "496286d2299947289df49c01001af364",
 })
 
